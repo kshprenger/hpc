646ea3cc864933ad4e42bad69e50cb7265ea50ac
Radu-Mihai Costache <radu.mihai.costache1@huawei.com>
2026-02-23 12:08:04 +0100

    openMP on regions

--- include/region_filter.h
+++ include/region_filter.h
@@ -6,6 +6,7 @@
 #include "split.h"
 #include <math.h>
 #include <mpi.h>
+#include <omp.h>
 #include <stdlib.h>
 
 #define GHOST_WIDTH 5
@@ -17,6 +18,8 @@ static inline void apply_gray_filter_to_region(Region *region) {
   }
 
   int total_pixels = region->region_width * region->region_height;
+
+  #pragma omp parallel for schedule(static)
   for (int j = 0; j < total_pixels; j++) {
     int moy = (region->p[j].r + region->p[j].g + region->p[j].b) / 3;
     if (moy < 0)
@@ -36,74 +39,82 @@ static inline int blur_iteration(Region *region, pixel *new_pixels, int size,
   int width = region->region_width;
   int height = region->region_height;
   pixel *p = region->p;
+  const int denom = (2 * size + 1) * (2 * size + 1);
+
+  #pragma omp parallel for collapse(2) schedule(static)
+  for (int j = 0; j < height; j++) {
+    for (int k = 0; k < width; k++) {
+      new_pixels[CONV(j, k, width)] = p[CONV(j, k, width)];
+    }
+  }
+
+  int x0 = GHOST_WIDTH;
+  int x1 = width - GHOST_WIDTH;
+
+  int blur_x0 = (x0 > size) ? x0 : size;
+  int blur_x1 = (x1 < width - size) ? x1 : (width - size);
+
+  #pragma omp parallel for collapse(2) schedule(static)
+  for (int j = size; j < height / 10 - size; j++) {
+    for (int k = blur_x0; k < blur_x1; k++) {
+      int t_r = 0, t_g = 0, t_b = 0;
+
+      for (int stencil_j = -size; stencil_j <= size; stencil_j++) {
+        for (int stencil_k = -size; stencil_k <= size; stencil_k++) {
+          pixel q = p[CONV(j + stencil_j, k + stencil_k, width)];
+          t_r += q.r; t_g += q.g; t_b += q.b;
+        }
+      }
+
+      int idx = CONV(j, k, width);
+      new_pixels[idx].r = (unsigned char)(t_r / denom);
+      new_pixels[idx].g = (unsigned char)(t_g / denom);
+      new_pixels[idx].b = (unsigned char)(t_b / denom);
+    }
+  }
+
+  #pragma omp parallel for collapse(2) schedule(static)
+  for (int j = (int)(height * 0.9) + size; j < height - size; j++) {
+    for (int k = blur_x0; k < blur_x1; k++) {
+      int t_r = 0, t_g = 0, t_b = 0;
+
+      for (int stencil_j = -size; stencil_j <= size; stencil_j++) {
+        for (int stencil_k = -size; stencil_k <= size; stencil_k++) {
+          pixel q = p[CONV(j + stencil_j, k + stencil_k, width)];
+          t_r += q.r; t_g += q.g; t_b += q.b;
+        }
+      }
+
+      int idx = CONV(j, k, width);
+      new_pixels[idx].r = (unsigned char)(t_r / denom);
+      new_pixels[idx].g = (unsigned char)(t_g / denom);
+      new_pixels[idx].b = (unsigned char)(t_b / denom);
+    }
+  }
+
   int local_end = 1;
 
-  for (int j = 0; j < height - 1; j++) {
-    for (int k = 0; k < width - 1; k++) {
-      new_pixels[CONV(j, k, width)].r = p[CONV(j, k, width)].r;
-      new_pixels[CONV(j, k, width)].g = p[CONV(j, k, width)].g;
-      new_pixels[CONV(j, k, width)].b = p[CONV(j, k, width)].b;
-    }
-  }
-
-  for (int j = size; j < height / 10 - size; j++) {
-    for (int k = size; k < width - size; k++) {
-      int t_r = 0, t_g = 0, t_b = 0;
-
-      for (int stencil_j = -size; stencil_j <= size; stencil_j++) {
-        for (int stencil_k = -size; stencil_k <= size; stencil_k++) {
-          t_r += p[CONV(j + stencil_j, k + stencil_k, width)].r;
-          t_g += p[CONV(j + stencil_j, k + stencil_k, width)].g;
-          t_b += p[CONV(j + stencil_j, k + stencil_k, width)].b;
-        }
-      }
-
-      new_pixels[CONV(j, k, width)].r = t_r / ((2 * size + 1) * (2 * size + 1));
-      new_pixels[CONV(j, k, width)].g = t_g / ((2 * size + 1) * (2 * size + 1));
-      new_pixels[CONV(j, k, width)].b = t_b / ((2 * size + 1) * (2 * size + 1));
-    }
-  }
-
-  for (int j = height / 10 - size; j < height * 0.9 + size; j++) {
-    for (int k = size; k < width - size; k++) {
-      new_pixels[CONV(j, k, width)].r = p[CONV(j, k, width)].r;
-      new_pixels[CONV(j, k, width)].g = p[CONV(j, k, width)].g;
-      new_pixels[CONV(j, k, width)].b = p[CONV(j, k, width)].b;
-    }
-  }
-
-  for (int j = height * 0.9 + size; j < height - size; j++) {
-    for (int k = size; k < width - size; k++) {
-      int t_r = 0, t_g = 0, t_b = 0;
-
-      for (int stencil_j = -size; stencil_j <= size; stencil_j++) {
-        for (int stencil_k = -size; stencil_k <= size; stencil_k++) {
-          t_r += p[CONV(j + stencil_j, k + stencil_k, width)].r;
-          t_g += p[CONV(j + stencil_j, k + stencil_k, width)].g;
-          t_b += p[CONV(j + stencil_j, k + stencil_k, width)].b;
-        }
-      }
-
-      new_pixels[CONV(j, k, width)].r = t_r / ((2 * size + 1) * (2 * size + 1));
-      new_pixels[CONV(j, k, width)].g = t_g / ((2 * size + 1) * (2 * size + 1));
-      new_pixels[CONV(j, k, width)].b = t_b / ((2 * size + 1) * (2 * size + 1));
-    }
-  }
-
+  #pragma omp parallel for collapse(2) reduction(&&:local_end) schedule(static)
   for (int j = 1; j < height - 1; j++) {
-    for (int k = 1; k < width - 1; k++) {
-      float diff_r = new_pixels[CONV(j, k, width)].r - p[CONV(j, k, width)].r;
-      float diff_g = new_pixels[CONV(j, k, width)].g - p[CONV(j, k, width)].g;
-      float diff_b = new_pixels[CONV(j, k, width)].b - p[CONV(j, k, width)].b;
+    for (int k = x0; k < x1; k++) {
+      int idx = CONV(j, k, width);
 
-      if (diff_r > threshold || -diff_r > threshold || diff_g > threshold ||
-          -diff_g > threshold || diff_b > threshold || -diff_b > threshold) {
-        local_end = 0;
-      }
+      float diff_r = (float)new_pixels[idx].r - (float)p[idx].r;
+      float diff_g = (float)new_pixels[idx].g - (float)p[idx].g;
+      float diff_b = (float)new_pixels[idx].b - (float)p[idx].b;
 
-      p[CONV(j, k, width)].r = new_pixels[CONV(j, k, width)].r;
-      p[CONV(j, k, width)].g = new_pixels[CONV(j, k, width)].g;
-      p[CONV(j, k, width)].b = new_pixels[CONV(j, k, width)].b;
+      int ok = !(diff_r > threshold || -diff_r > threshold ||
+                 diff_g > threshold || -diff_g > threshold ||
+                 diff_b > threshold || -diff_b > threshold);
+
+      local_end = local_end && ok;
+    }
+  }
+
+  #pragma omp parallel for collapse(2) schedule(static)
+  for (int j = 1; j < height - 1; j++) {
+    for (int k = x0; k < x1; k++) {
+      p[CONV(j, k, width)] = new_pixels[CONV(j, k, width)];
     }
   }
 
@@ -268,43 +279,40 @@ static inline void apply_sobel_filter_to_region(Region *region) {
     return;
   }
 
+  #pragma omp parallel for collapse(2) schedule(static)
   for (int j = 1; j < height - 1; j++) {
     for (int k = 1; k < width - 1; k++) {
       int pixel_blue_no = p[CONV(j - 1, k - 1, width)].b;
-      int pixel_blue_n = p[CONV(j - 1, k, width)].b;
+      int pixel_blue_n  = p[CONV(j - 1, k,     width)].b;
       int pixel_blue_ne = p[CONV(j - 1, k + 1, width)].b;
       int pixel_blue_so = p[CONV(j + 1, k - 1, width)].b;
-      int pixel_blue_s = p[CONV(j + 1, k, width)].b;
+      int pixel_blue_s  = p[CONV(j + 1, k,     width)].b;
       int pixel_blue_se = p[CONV(j + 1, k + 1, width)].b;
-      int pixel_blue_o = p[CONV(j, k - 1, width)].b;
-      int pixel_blue_e = p[CONV(j, k + 1, width)].b;
+      int pixel_blue_o  = p[CONV(j,     k - 1, width)].b;
+      int pixel_blue_e  = p[CONV(j,     k + 1, width)].b;
 
-      float deltaX_blue = -pixel_blue_no + pixel_blue_ne - 2 * pixel_blue_o +
-                          2 * pixel_blue_e - pixel_blue_so + pixel_blue_se;
+      float deltaX_blue = -pixel_blue_no + pixel_blue_ne
+                          - 2.0f * pixel_blue_o + 2.0f * pixel_blue_e
+                          - pixel_blue_so + pixel_blue_se;
 
-      float deltaY_blue = pixel_blue_se + 2 * pixel_blue_s + pixel_blue_so -
-                          pixel_blue_ne - 2 * pixel_blue_n - pixel_blue_no;
+      float deltaY_blue =  pixel_blue_se + 2.0f * pixel_blue_s + pixel_blue_so
+                          - pixel_blue_ne - 2.0f * pixel_blue_n - pixel_blue_no;
 
-      float val_blue =
-          sqrt(deltaX_blue * deltaX_blue + deltaY_blue * deltaY_blue) / 4;
+      float val_blue = sqrtf(deltaX_blue * deltaX_blue +
+                             deltaY_blue * deltaY_blue) / 4.0f;
 
-      if (val_blue > 50) {
-        sobel[CONV(j, k, width)].r = 255;
-        sobel[CONV(j, k, width)].g = 255;
-        sobel[CONV(j, k, width)].b = 255;
-      } else {
-        sobel[CONV(j, k, width)].r = 0;
-        sobel[CONV(j, k, width)].g = 0;
-        sobel[CONV(j, k, width)].b = 0;
-      }
+      pixel out = (val_blue > 50.0f) ? (pixel){255,255,255} : (pixel){0,0,0};
+      sobel[CONV(j, k, width)] = out;
     }
   }
 
+  int x0 = GHOST_WIDTH;
+  int x1 = width - GHOST_WIDTH;
+
+  #pragma omp parallel for collapse(2) schedule(static)
   for (int j = 1; j < height - 1; j++) {
-    for (int k = 1; k < width - 1; k++) {
-      p[CONV(j, k, width)].r = sobel[CONV(j, k, width)].r;
-      p[CONV(j, k, width)].g = sobel[CONV(j, k, width)].g;
-      p[CONV(j, k, width)].b = sobel[CONV(j, k, width)].b;
+    for (int k = x0; k < x1; k++) {
+      p[CONV(j, k, width)] = sobel[CONV(j, k, width)];
     }
   }
 
